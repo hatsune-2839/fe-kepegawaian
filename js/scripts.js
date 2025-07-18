b573254009ad90c914526d5827125b116fd3c7e5
HSN <github.hatsune_2839@protonmail.com>
2025-07-18 23:23:37 +0800

    chore: restructure project folders and move files

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,5 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const sidebar = document.querySelector(".sidebar");
-    const toggleButton = document.getElementById("toggle-sidebar");
-
-    toggleButton.addEventListener("click", () => {
-        sidebar.classList.toggle("collapsed");
+$(document).ready(function () {
+    $("#toggle-sidebar").click(function () {
+        $(".sidebar").toggleClass("collapsed");
     });
 });
